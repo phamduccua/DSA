a61aaa9e391fd90922219eeaf49a34ad548ea2ed
phamduccua <phamcua84@gmail.com>
2025-04-08 11:00:32 +0700

    a

--- dau  ngoac dung dai nhat code ptit.cpp
+++ dau  ngoac dung dai nhat code ptit.cpp
@@ -1,33 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
-	int dem(string s,stack<char> st){
-		int cnt = 0;
-		int maxx = INT_MIN;
-		for(int i = 0;i<s.length();i++){
-			if(s[i] == '(') st.push(s[i]);
-			else if(s[i] == ')'){
-				if(!st.empty()){
-					cnt += 2;
-					st.pop();
-				}
-				else{
-					maxx = max(maxx,cnt);
-					cnt = 0;
-					while(!st.empty()){
-						st.pop();
-					} 
-				}
-			}
-			maxx = max(maxx,cnt);
-		}
-		return maxx;
-	}
-	int main(){
-		int t; cin >> t;
-		while(t--){
-			string s; cin >> s;
-			stack<char> st;
-			int count = dem(s,st);
-			cout << count << endl;
-		}
-	}
+
+int solve(string s) {
+    stack<int> st;
+    st.push(-1);  // Đẩy một giá trị mặc định vào để tính toán chiều dài
+    int ans = 0;
+
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == '(') {
+            st.push(i); 
+        } else {
+            st.pop();  
+            if (!st.empty()) {
+                ans = max(ans, i - st.top());
+            } else {
+                st.push(i);
+            }
+        }
+    }
+
+    return ans;
+}
+
+int main() {
+    int t; 
+    cin >> t;
+    while (t--) {
+        string s; 
+        cin >> s;
+        cout << solve(s) << endl;
+    }
+}
