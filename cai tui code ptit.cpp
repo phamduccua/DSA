4d6dc17dda831d72e206eea81d4199145a4cda1e
phamduccua <phamcua84@gmail.com>
2025-04-29 10:43:38 +0700

    a

--- cai tui code ptit.cpp
+++ cai tui code ptit.cpp
@@ -1,21 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 int dp[1001][1001];
-	int main(){
-		int t; cin >> t;
-		while(t--){
-			int n,V; cin >> n >> V;
-			memset(dp,0,sizeof(dp));
-			int a[n+1],c[n+1];
-			for(int i = 1;i<=n;i++) cin >> a[i];
-			for(int i = 1;i<=n;i++) cin >> c[i];
-			for(int i = 1;i<=n;i++){
-				for(int x = 0;x<=V;x++){
-					if(x >= a[i]){
-						dp[i][x] = max(dp[i-1][x],dp[i-1][x-a[i]]+c[i]);
-					}
-				}
-			}
-			cout << dp[n][V] << endl;
-		}
-	}
+    int main(){
+        int t; cin >> t;
+        while(t--){
+            memset(dp,0,sizeof(dp));
+            int n,V; cin >> n >> V;
+            int w[n+5],c[n+5];
+            for(int i = 1;i <= n;i++){
+                cin >> w[i];
+            }
+            for(int i = 1;i <= n;i++){
+                cin >> c[i];
+            }
+            for(int i = 1;i <= n;i++){
+                for(int v = 0;v <= V; v++){
+                    dp[i][v] = dp[i-1][v];
+                    if(v >= w[i]){
+                        dp[i][v] = max(dp[i-1][v], dp[i-1][v - w[i]] + c[i]);
+                    }
+                }
+            }
+            cout << dp[n][V] << endl;
+        }
+    }
